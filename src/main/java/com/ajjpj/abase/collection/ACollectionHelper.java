04e1a259945113e0911d534ea46c76f936e30c6f
Arno Haase <arno.haase@haase-consulting.com>
2014-03-14 23:36:06 +0100

    added junit tests

--- src/main/java/com/ajjpj/abase/collection/ACollectionHelper.java
+++ src/main/java/com/ajjpj/abase/collection/ACollectionHelper.java
@@ -49,7 +49,10 @@ public class ACollectionHelper {
         return result.toString();
     }
 
-    public static <T, E extends Exception> AOption<T> find(Iterable<T> coll, APredicate<T, E> pred) throws E { //TODO junit, use in alist etc.
+    /**
+     * Returns an element of a collection that matches a predicate, if any, or AOption.none() if there is no match.
+     */
+    public static <T, E extends Exception> AOption<T> find(Iterable<T> coll, APredicate<T, E> pred) throws E {
         for(T o: coll) {
             if(pred.apply(o)) {
                 return AOption.some(o);
@@ -58,7 +61,10 @@ public class ACollectionHelper {
         return AOption.none();
     }
 
-    public static <T, E extends Exception> boolean forAll(Iterable<T> coll, APredicate<T, E> pred) throws E { //TODO junit
+    /**
+     * Matches a predicate against collection elements, and returns true iff it matches them all.
+     */
+    public static <T, E extends Exception> boolean forAll(Iterable<T> coll, APredicate<T, E> pred) throws E {
         for(T o: coll) {
             if(!pred.apply(o)) {
                 return false;
@@ -67,7 +73,10 @@ public class ACollectionHelper {
         return true;
     }
 
-    public static <T, E extends Exception> boolean exists(Iterable<T> coll, APredicate<T, E> pred) throws E { //TODO junit
+    /**
+     * Matches a predicate against collection elements, and returns true iff it matches at least one of them.
+     */
+    public static <T, E extends Exception> boolean exists(Iterable<T> coll, APredicate<T, E> pred) throws E {
         for(T o: coll) {
             if(pred.apply(o)) {
                 return true;
@@ -76,7 +85,10 @@ public class ACollectionHelper {
         return false;
     }
 
-    public static <T, X, E extends Exception> Collection<X> map(Iterable<T> coll, AFunction1<X, T, E> f) throws E { //TODO junit, javadoc
+    /**
+     * Applies a transformation function to all elements of a collection, creating a new collection from the results.
+     */
+    public static <T, X, E extends Exception> Collection<X> map(Iterable<T> coll, AFunction1<X, T, E> f) throws E {
         final List<X> result = new ArrayList<>();
 
         for(T o: coll) {
@@ -86,7 +98,11 @@ public class ACollectionHelper {
         return result;
     }
 
-    public static <T, X, E extends Exception> Collection<X> flatMap(Iterable<T> coll, AFunction1<Iterable<X>, T, E> f) throws E { //TODO junit, javadoc
+    /**
+     * Same as <code>map()</code>, except that the transformation function returns collections and all the results are
+     *  flattened into a single collection.
+     */
+    public static <T, X, E extends Exception> Collection<X> flatMap(Iterable<T> coll, AFunction1<? extends Iterable<X>, T, E> f) throws E {
         final List<X> result = new ArrayList<>();
 
         for(T o: coll) {
@@ -98,7 +114,11 @@ public class ACollectionHelper {
         return result;
     }
 
-    public static <T> Collection<T> flatten(Iterable<? extends Iterable<T>> coll) { //TODO junit, javadoc
+    /**
+     * Takes a collection of collections and creates a new collection from the elements, leaving out the innermost
+     *  level of collection.
+     */
+    public static <T> Collection<T> flatten(Iterable<? extends Iterable<T>> coll) {
         final List<T> result = new ArrayList<>();
         for(Iterable<T> o: coll) {
             for(T el: o) {
@@ -108,7 +128,10 @@ public class ACollectionHelper {
         return result;
     }
 
-    public static <T, E extends Exception> Collection<T> filter(Iterable<T> coll, APredicate<T, E> pred) throws E { //TODO junit
+    /**
+     * Matches all elements of a collection against a predicate, creating a new collection from those that match.
+     */
+    public static <T, E extends Exception> Collection<T> filter(Iterable<T> coll, APredicate<T, E> pred) throws E {
         final List<T> result = new ArrayList<>();
         for(T o: coll) {
             if(pred.apply(o)) {
@@ -118,7 +141,11 @@ public class ACollectionHelper {
         return result;
     }
 
-    public static <T, X, E extends Exception> Map<X, Collection<T>> groupBy(Iterable<T> coll, AFunction1<X, T, E> f) throws E { //TOD javadoc, junit
+    /**
+     * Creates a Map from a collection. Each element's key is determined by applying a function to the element. All
+     *  elements with the same key are stored as that key's value in the returned Map.
+     */
+    public static <T, X, E extends Exception> Map<X, Collection<T>> groupBy(Iterable<T> coll, AFunction1<X, T, E> f) throws E {
         final Map<X, Collection<T>> result = new HashMap<>();
         for(T o: coll) {
             final X key = f.apply(o);
@@ -132,7 +159,17 @@ public class ACollectionHelper {
         return result;
     }
 
-    public static <T, X, E extends Exception> Map<AEqualsWrapper<X>, Collection<T>> groupBy(Iterable<T> coll, AFunction1<X, T, E> f, AEquality keyEquality) throws E { //TODO javadoc, junit
+    /**
+     * Creates a Map from a collection. Each element's key is determined by applying a function to the element. All
+     *  elements with the same key are stored as that key's value in the returned Map.<p />
+     *
+     * This method gives control over the equality strategy used to determine if two keys are 'equal'. To accomodate that,
+     *  the keys are wrapped in AEqualsWrapper. <p />
+     *
+     * This method is rather technical in nature, and it is probably more useful as a foundation for generic code than
+     *  for direct use by applications.
+     */
+    public static <T, X, E extends Exception> Map<AEqualsWrapper<X>, Collection<T>> groupBy(Iterable<T> coll, AFunction1<X, T, E> f, AEquality keyEquality) throws E {
         final Map<AEqualsWrapper<X>, Collection<T>> result = new HashMap<>();
         for(T o: coll) {
             final AEqualsWrapper<X> key = new AEqualsWrapper<>(keyEquality, f.apply(o));
--- src/test/java/com/ajjpj/abase/collection/ACollectionHelperTest.java
+++ src/test/java/com/ajjpj/abase/collection/ACollectionHelperTest.java
@@ -1,14 +1,132 @@
 package com.ajjpj.abase.collection;
 
+import com.ajjpj.abase.collection.immutable.AOption;
+import com.ajjpj.abase.function.AFunction1NoThrow;
+import com.ajjpj.abase.function.APredicateNoThrow;
+import org.junit.Ignore;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 import java.util.*;
+
+import static org.junit.Assert.*;
+
 
 /**
  * @author arno
  */
 public class ACollectionHelperTest {
+    @Test
+    public void testMkStringNoArgs() {
+        assertEquals("",        ACollectionHelper.mkString(Arrays.asList()));
+        assertEquals("a",       ACollectionHelper.mkString(Arrays.asList("a")));
+        assertEquals("a, b, c", ACollectionHelper.mkString(Arrays.asList("a", "b", "c")));
+    }
+
+    @Test
+    public void testMkStringSeparator() {
+        assertEquals("",      ACollectionHelper.mkString(Arrays.asList(),              "#"));
+        assertEquals("a",     ACollectionHelper.mkString(Arrays.asList("a"),           "#"));
+        assertEquals("a#b#c", ACollectionHelper.mkString(Arrays.asList("a", "b", "c"), "#"));
+
+        assertEquals("",        ACollectionHelper.mkString(Arrays.asList(),              "?!"));
+        assertEquals("a",       ACollectionHelper.mkString(Arrays.asList("a"),           "?!"));
+        assertEquals("a?!b?!c", ACollectionHelper.mkString(Arrays.asList("a", "b", "c"), "?!"));
+    }
+
+    @Test
+    public void testMkStringFull() {
+        assertEquals("[]",      ACollectionHelper.mkString(Arrays.asList(),              "[", "#", "]"));
+        assertEquals("[a]",     ACollectionHelper.mkString(Arrays.asList("a"),           "[", "#", "]"));
+        assertEquals("[a#b#c]", ACollectionHelper.mkString(Arrays.asList("a", "b", "c"), "[", "#", "]"));
+
+        assertEquals("<<>>",        ACollectionHelper.mkString(Arrays.asList(),              "<<", "?!", ">>"));
+        assertEquals("<<a>>",       ACollectionHelper.mkString(Arrays.asList("a"),           "<<", "?!", ">>"));
+        assertEquals("<<a?!b?!c>>", ACollectionHelper.mkString(Arrays.asList("a", "b", "c"), "<<", "?!", ">>"));
+    }
+
+    @Test
+    public void testFind() {
+        final APredicateNoThrow<String> len1 = new APredicateNoThrow<String>() {
+            @Override  public boolean apply(String o) {
+                return o.length() == 1;
+            }
+        };
+
+        assertEquals (AOption.<String>none(), ACollectionHelper.find(Arrays.<String>asList(), len1));
+        assertEquals (AOption.<String>none(), ACollectionHelper.find(Arrays.asList("", "ab", "cde"), len1));
+        assertEquals (AOption.some("d"), ACollectionHelper.find(Arrays.asList("", "abc", "d", "ef", "g"), len1));
+    }
+
+    @Test
+    public void testForAll() {
+        final APredicateNoThrow<String> len1 = new APredicateNoThrow<String>() {
+            @Override  public boolean apply(String o) {
+                return o.length() == 1;
+            }
+        };
+
+        assertEquals(true, ACollectionHelper.forAll(Arrays.<String>asList(), len1));
+        assertEquals(true, ACollectionHelper.forAll(Arrays.asList("a"), len1));
+        assertEquals(true, ACollectionHelper.forAll(Arrays.asList("a", "b", "c"), len1));
+        assertEquals(false, ACollectionHelper.forAll(Arrays.asList("a", "b", "cd", "e"), len1));
+    }
+
+    @Test
+    public void testExists() {
+        final APredicateNoThrow<String> len1 = new APredicateNoThrow<String>() {
+            @Override  public boolean apply(String o) {
+                return o.length() == 1;
+            }
+        };
+
+        assertEquals(false, ACollectionHelper.exists(Arrays.<String>asList(), len1));
+        assertEquals(true, ACollectionHelper.exists(Arrays.asList("a"), len1));
+        assertEquals(false, ACollectionHelper.exists(Arrays.asList("ab"), len1));
+        assertEquals(true, ACollectionHelper.exists(Arrays.asList("ab", "c", "def"), len1));
+    }
+
+    @Test
+    public void testMap() {
+        final AFunction1NoThrow<Integer, String> len = new AFunction1NoThrow<Integer, String>() {
+            @Override public Integer apply(String param) {
+                return param.length();
+            }
+        };
+
+        assertEquals(Arrays.<Integer>asList(), ACollectionHelper.map(Arrays.<String>asList(), len));
+        assertEquals(Arrays.asList(1), ACollectionHelper.map(Arrays.asList("a"), len));
+        assertEquals(Arrays.asList(2, 1, 3), ACollectionHelper.map(Arrays.asList("ab", "c", "def"), len));
+    }
+
+    @Test
+    public void testFlatMapTokens() {
+        final AFunction1NoThrow<List<String>, String> tokens = new AFunction1NoThrow<List<String>, String>() {
+            @Override public List<String> apply(String param) {
+                return Arrays.asList(param.split(" "));
+            }
+        };
+
+        assertEquals(Arrays.<String>asList(), ACollectionHelper.flatMap(Arrays.<String>asList(), tokens));
+        assertEquals(Arrays.asList("a"), ACollectionHelper.flatMap(Arrays.asList("a"), tokens));
+        assertEquals(Arrays.asList("a", "bc", "def"), ACollectionHelper.flatMap(Arrays.asList("a bc def"), tokens));
+        assertEquals(Arrays.asList("a", "bc", "def", "x", "yz"), ACollectionHelper.flatMap(Arrays.asList("a bc def", "x yz"), tokens));
+    }
+
+    @Test
+    public void testFlatMapOption() {
+        final AFunction1NoThrow<AOption<String>, String> uppercaseFirst = new AFunction1NoThrow<AOption<String>, String>() {
+            @Override public AOption<String> apply(String param) {
+                if(Character.isUpperCase(param.charAt(0)))
+                    return AOption.some(param.substring(0, 1));
+                return AOption.none();
+            }
+        };
+
+        assertEquals(Arrays.<String>asList(), ACollectionHelper.flatMap(Arrays.<String>asList(), uppercaseFirst));
+        assertEquals(Arrays.asList("A"), ACollectionHelper.flatMap(Arrays.asList("Asdf"), uppercaseFirst));
+        assertEquals(Arrays.asList("A", "Q"), ACollectionHelper.flatMap(Arrays.asList("xyz", "Asdf", "Qzd", "rLS"), uppercaseFirst));
+    }
+
     @Test
     public void testFlatten() {
         final Set<Set<String>> set = new HashSet<>();
@@ -22,5 +140,31 @@ public class ACollectionHelperTest {
         final List<String> flattenedList = new ArrayList<>(flattened);
         Collections.sort(flattenedList);
         assertEquals(Arrays.asList("a", "b", "b", "c", "d"), flattenedList);
+    }
+
+    @Test
+    public void testFilter() {
+        final APredicateNoThrow<String> len1 = new APredicateNoThrow<String>() {
+            @Override  public boolean apply(String o) {
+                return o.length() == 1;
+            }
+        };
+
+        assertEquals(Arrays.<String>asList(), ACollectionHelper.filter(Arrays.<String>asList(), len1));
+        assertEquals(Arrays.<String>asList(), ACollectionHelper.filter(Arrays.asList("abc"), len1));
+        assertEquals(Arrays.asList("a"), ACollectionHelper.filter(Arrays.asList("a"), len1));
+        assertEquals(Arrays.asList("a", "d"), ACollectionHelper.filter(Arrays.asList("a", "bc", "d", "efg"), len1));
+    }
+
+    @Test
+    @Ignore
+    public void testGroupBy() {
+        fail("todo (2x)");
+    }
+
+    @Test
+    @Ignore
+    public void testAsCollectionCopy() {
+        fail("todo");
     }
 }
