7ad4f9a4a22af02d02b32c29a2ffecabd5df932b
Arno Haase <arno.haase@haase-consulting.com>
2014-03-17 18:31:30 +0100

    cleanup

--- src/main/java/com/ajjpj/abase/function/AFunction0.java
+++ src/main/java/com/ajjpj/abase/function/AFunction0.java
@@ -6,15 +6,3 @@ package com.ajjpj.abase.function;
 public interface AFunction0<R, E extends Exception> {
     R apply() throws E;
 }
-
-interface A <X extends A<X>> {
-
-}
-
-interface A0 extends A<A0> {
-
-}
-
-class B implements A0 {
-
-}
