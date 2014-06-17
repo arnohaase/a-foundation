699834121a2b4c3079ed832ce1020bc76eb177f8
BitMagier <feuchtfuss@gmail.com>
2014-06-18 00:00:31 +0200

    changed to java convention type parameter names

--- src/main/java/com/ajjpj/abase/function/AFunction2.java
+++ src/main/java/com/ajjpj/abase/function/AFunction2.java
@@ -4,11 +4,11 @@ package com.ajjpj.abase.function;
  * Represents a function that takes two arguments and produces a result.
  *
  * @param <T> 1st parameter type
- * @param <X> 2nd parameter type
+ * @param <S> 2nd parameter type
  * @param <R> return type
  *
  * @author bitmagier
  */
-public interface AFunction2<T, X, R, E extends Exception> {
-    R apply (T param1, X param2) throws E;
+public interface AFunction2<T, S, R, E extends Exception> {
+    R apply (T param1, S param2) throws E;
 }
